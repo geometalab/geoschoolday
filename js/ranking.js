40ae240b4140f1c70e0b788c6e72dfab3bd9211c
Joël Schwab <jschwab@hsr.ch>
2016-06-09 16:38:54 +0200

    Ranking

--- js/ranking.js
+++ js/ranking.js
@@ -1,17 +1,17 @@
 function firstRank(){
     var button = document.getElementById("button1");
-    button.textContent = "1. ";
+    button.textContent = "1. Kantonsschule Hohe Promenade: 4c - Zürich";
     button.disabled = true;
 }
 
 function secondRank(){
     var button = document.getElementById("button2");
-    button.textContent = "2. ";
+    button.textContent = "2. Gymnasium Kirchenfeld: G3e - Bern";
     button.disabled = true;
 }
 
 function thirdRank(){
     var button = document.getElementById("button3");
-    button.textContent = "3. ";
+    button.textContent = "3. Gymnasium Kirchenfeld: M3d - Bern";
     button.disabled = true;
 }
